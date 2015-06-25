8a4a94e2590ffd0a233bced3f6df873b1d4070c0
Stan Bondi <stan@fixate.it>
2015-06-25 11:12:28 +0200

    install script improvement, added vagrant-scp script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+cd "$(dirname "${BASH_SOURCE}")"
+
+rsync_dotfiles() {
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "install.sh" --exclude "deps.sh" \
+    --exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+  source ~/.bash_profile
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+  rsync_dotfiles
+else
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    rsync_dotfiles
+  fi
+fi
+

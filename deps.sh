c740fd3cb91acae351e7d8417ec96a875008fbcb
Stan Bondi <stan@fixate.it>
2016-03-13 14:50:10 +0200

    Remove stuff from deps which i dont need/want

--- deps.sh
+++ deps.sh
@@ -1,20 +1,5 @@
 # up to you (me) if you want to run this as a file or copy paste at your leisure
 
-
-# https://github.com/jamiew/git-friendly
-# the `push` command which copies the github compare URL to my clipboard is heaven
-sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
-
-# https://github.com/isaacs/nave
-# needs npm, obviously.
-# TODO: I think i'd rather curl down the nave.sh, symlink it into /bin and use that for initial node install.
-npm install -g nave
-
-
-# homebrew!
-# you need the code CLI tools YOU FOOL.
-ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
-
 # make a code directory for dependencies
 if [ ! -d ~/code ]; then
         mkdir ~/code
@@ -42,31 +27,8 @@ chmod -X ~/code/git-completion/git-completion.bash
 # git-completion binary is already referenced from .bash_profile
 
 
-# https://github.com/dronir/SpotifyControl
-# Spotify Control Script
-cd ~/code
-git clone git://github.com/dronir/SpotifyControl.git
-
-# https://github.com/jeroenbegyn/VLCControl
-# VLC Controll Script
-cd ~/code
-git clone git://github.com/jeroenbegyn/VLCControl.git
-
 # setup Vundle for Vim package management - https://github.com/gmarik/vundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
-# my magic photobooth symlink -> dropbox. I love it.
-# first move Photo Booth folder out of Pictures
-# then start Photo Booth. It'll ask where to put the library.
-# put it in Dropbox/public
-
-# now you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
-# then you grab public URL and send off your video message in a heartbeat.
 
 
-# for the c alias (syntax highlighted cat)
-sudo easy_install Pygments
-
-
-# chrome canary as default
-# on a mac you can set chrome canary as your default inside of Safari preferences :)
